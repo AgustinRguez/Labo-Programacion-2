87f0971978290e2bd9fd735f8260fbd1a287b00c
AgustinRguez <agus_rodriguez-@hotmail.com>
2021-04-05 14:16:13 -0300

    Cuarta clase: sobrecargas

---
+++ CuartaClaseProgLab2/Biblioteca/Kilometros.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Kilometros
+    {
+        public double cantidadKilometros;
+
+        private Kilometros(double cantidadKm) 
+        {
+            this.cantidadKilometros = cantidadKm;
+        }
+
+        public static implicit operator Kilometros(int numero) //implicita no hay perdida de datos
+        {
+            Kilometros kilometroUno = new Kilometros(numero);
+            return kilometroUno;
+        }
+    }
+}
